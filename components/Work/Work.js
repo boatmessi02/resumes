c33ddf0e23cf2215c6a3bba44a6d5df2448f2552
Mrboatiot <52354253+boatmessi02@users.noreply.github.com>
2022-12-03 08:42:25 +0700

    update folders work

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import WorkBuddy from "./WorkBuddyToTravel/WorkBuddy";
-import WorkOpenHouses from "./WorkOpenHouse/WorkOpenHouses";
-import WorkRandom from "./WorkRandom/WorkRandom";
+import WorkBuddy from "./WorkExperience/WorkBuddyToTravel/WorkBuddy";
+import WorkOpenHouses from "./WorkActivities/WorkOpenHouse/WorkOpenHouses";
+import WorkRandom from "./WorkExperience/WorkRandom/WorkRandom";
 
 const Work = () => {
   return (
